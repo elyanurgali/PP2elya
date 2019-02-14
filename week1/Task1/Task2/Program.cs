276e9df0f6d09d58232045978d43f5f5faa1879e
elyanurgali <elekonsuper_2000@mail.ru>
2019-02-15 03:22:34 +0600

    name,id,year

--- week1/Task1/Task2/Program.cs
+++ week1/Task1/Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,38 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class Student
         {
+            public string name;
+            public string id;
+            public string year;
+
+            public Student (string name, string id, int year)
+            {
+                this.name = name;
+                this.id = id;
+            
+            }
+
+            public void Print()
+            {
+                int year = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    year++;
+                    Console.WriteLine("NAME:" + name + " " + "ID:" + id + " " + "Year of study:"  + year + " ");
+                }
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Student s = new Student ("Nurgali Eleonora", "18BD111398" , 0);
+
+                s.Print();
+            }
         }
     }
-}
